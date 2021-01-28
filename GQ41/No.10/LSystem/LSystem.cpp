3a1be2565f4b91f5f726033849b512dc9377f88a
kamiryou3753 <kamiryou3753@gmail.com>
2021-01-28 10:43:29 +0900

    前方宣言の追加

--- GQ41/No.10/LSystem/LSystem.cpp
+++ GQ41/No.10/LSystem/LSystem.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 // グローバル変数
-string g_v;		// Vの値(出力される文字列)
-string g_s;		// Sの値(計算が進んでも変化しない定数の集合)
-string g_w;		// ωの値(初期の文字列)
-string g_p;		// Pの値(V を変化させてゆく置換規則の集合)
+string starttext;		// ωの値(初期の文字列)
+string drawtext;		// 最終的に出力される文字列
+
+int generation;			// 世代数
 
 // 前方宣言
 void Init();
 void Uninit();
+void Update();
+void Draw();
 
 int main()
 {
@@ -27,9 +29,19 @@ int main()
 
 void Init()
 {
-	g_s = "A";
+	
 }
 
 void Uninit()
 {
+}
+
+void Update()
+{
+
+}
+
+void Draw()
+{
+
 }
